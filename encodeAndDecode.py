5a50ecd19a339bd4503fb25722af68d8e1d5452b
soren-fliegel <83089360+soren-fliegel@users.noreply.github.com>
2024-01-23 21:24:41 -0800

    start of multi

--- encodeAndDecode.py
+++ encodeAndDecode.py
@@ -1,15 +1,20 @@
 import base64 
 import binascii
 
+encryptionType = "utf-8"
+#'ascii'
+handler = "replace"
+#'ignore'
+
 def base64Encode(input: str) -> str:
-    stringBytes = input.encode("ascii")
+    stringBytes = input.encode(encryptionType, handler)
     encodedBytes = base64.b64encode(stringBytes)
-    return encodedBytes.decode("ascii")
+    return encodedBytes.decode(encryptionType,handler)
 
 def base64Decode(input: str) -> str:
-    encodedBytes = input.encode("ascii")
+    encodedBytes = input.encode(encryptionType,handler)
     stringBytes = base64.b64decode(encodedBytes)
-    return stringBytes.decode("ascii")
+    return stringBytes.decode(encryptionType,handler)
 
 def convertToHex(binary: bytes) -> str:
     return str(binary.hex())
