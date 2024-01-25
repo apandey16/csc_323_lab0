3bfaa762e91f1c95d0b1081bcfe58cff24b633d5
apandey16 <apande02@calpoly.edu>
2024-01-25 11:22:50 -0800

    three possibilities, need to xor them against a value and then find retkey

--- multiByteXor.py
+++ multiByteXor.py
@@ -1,5 +1,7 @@
+import math
 from xor import xor
 from encodeAndDecode import base64Decode
+from singleByteXor import letterFrequency
 
 file = open("Encrypted Messages/Lab0.TaskII.C.txt", "r")
 lines = file.read()
@@ -10,42 +12,46 @@ def convertToStr(input) -> str:
     for letter in input:
         retStr += chr(letter)
     return retStr
-
-letterFrequency = {'E': 0.12003601080324099, 'T': 0.09102730819245775, 'A': 0.08122436731019306, 'O': 0.07682304691407423, 'I': 0.0731219365809743, 'N': 0.06952085625687708, 'S': 0.06281884565369612, 'R': 0.06021806541962589, 'H': 0.05921776532959889, 'D': 0.04321296388916676, 'L': 0.03981194358307493, 'U': 0.028808642592777836, 'C': 0.027108132439731925, 'M': 0.026107832349704915, 'F': 0.02300690207062119, 'Y': 0.021106331899569872, 'W': 0.02090627188156447, 'G': 0.020306091827548264, 'P': 0.01820546163849155, 'B': 0.014904471341402423, 'V': 0.011103330999299792, 'K': 0.006902070621186356, 'X': 0.0017005101530459142, 'Q': 0.0011003300990297092, 'J': 0.0010003000900270084, 'Z': 0.0007002100630189059}
-
 msg = convertToStr(base64Decode(lines))
-# print(base64Decode(lines))
+
+def score(msg):
+    msgLen = len(msg)
+
+    msgFrequency = {}
+
+    for char in msg:
+        if char in msgFrequency:
+            msgFrequency[char] += 1/msgLen
+        else:
+            msgFrequency[char] = 1/msgLen
+        
+    score = 0
+    for char in msgFrequency:
+        if char.upper() in letterFrequency:
+            score += math.fabs(msgFrequency[char] - letterFrequency[char.upper()])
+        else:
+            score += 1
+    
+    return score
 
 def keyLen(input):
     curKeyLen = None
-    curScore = None
+    curScore = 25
+    scoreLenDict = {}
     
-    inputLength = len(input)
 
-    # this should make the windows we are testing
-    for keySize in range(1, inputLength - 1):
-        window = []
-        for position in range(0,inputLength, keySize):
-            window.append(input[position: position + keySize])
-        # print(window)
-        
-        #now need to score, then check curKeyLen and replace as needed
-        
-
+    for keySize in range(2, 20):
+        curStr = ""
+        curStr = input[::keySize]
+        print(curStr)
+        print("score " + str(score(curStr)))
+        print()
     
-    # print(window)
+        if curScore > score(curStr):
+            curScore = score(curStr)
+            curKeyLen = keySize
+            scoreLenDict[curScore] = curKeyLen
+            
+    return scoreLenDict
 
-msgFrequency = {}
-# print(msg)
-# print(len(msg))
-
-for char in msg:
-    # print (char)
-    if char in msgFrequency:
-        msgFrequency[char] += 1/len(msg)
-    else:
-        msgFrequency[char] = 1/len(msg)
-
-# print(msgFrequency)
-# print(len(msgFrequency))
-keyLen(msg)
+print("retVal: " + str(keyLen(msg)))
--- singleByteXor.py
+++ singleByteXor.py
@@ -56,4 +56,5 @@ def main():
         else:
             break
 
-main()
+if __name__ == "__main__":
+        main()
