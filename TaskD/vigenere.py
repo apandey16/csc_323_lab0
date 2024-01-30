9faeae8856d91de915139551bc9253024032eed9
soren-fliegel <83089360+soren-fliegel@users.noreply.github.com>
2024-01-30 13:50:48 -0800

    got it!!!

--- TaskD/vigenere.py
+++ TaskD/vigenere.py
@@ -1,8 +1,6 @@
 # https://www.youtube.com/watch?v=QQhJOCfdRg4&ab_channel=LearningPython
 # https://www.youtube.com/watch?v=LaWp_Kq0cKs&ab_channel=Theoretically
-
 import math
-
 #need to change scoring function as the right ones aren't being passed
 def scoreMsg(msg: str) -> float:
 
@@ -27,6 +25,31 @@ def scoreMsg(msg: str) -> float:
             score += math.fabs(msgFrequency[char] - englishFrequency[char.upper()])
     
     return score
+
+def scoreMsgMult(windowFrequency: list, length: int) -> float:
+    englishFrequency = [('A', 0.08122436731019306),('B', 0.014904471341402423),('C', 0.027108132439731925),('D', 0.04321296388916676),('E', 0.12003601080324099),('F', 0.02300690207062119),
+    ('G', 0.020306091827548264),('H', 0.05921776532959889),('I', 0.0731219365809743),('J', 0.0010003000900270084),('K', 0.006902070621186356),('L', 0.03981194358307493),
+    ('M', 0.026107832349704915),('N', 0.06952085625687708),('O', 0.07682304691407423),('P', 0.01820546163849155),('Q', 0.0011003300990297092),('R', 0.06021806541962589),
+    ('S', 0.06281884565369612),('T', 0.09102730819245775),('U', 0.028808642592777836),('V', 0.011103330999299792),
+    ('W', 0.02090627188156447),('X', 0.0017005101530459142),('Y', 0.021106331899569872),('Z', 0.0007002100630189059)]
+
+    maxScore = 0 
+    maxShift = 0
+
+    for count in range(length):
+        score = 0
+        for i, tupley in enumerate(windowFrequency):
+            # here I am always comparing the same char together; need to be able to shift and compare various ones
+            # windowFrequency must change but engish should stay the sam eorder
+            score += windowFrequency[i][1] * englishFrequency[i][1]
+        
+        if score > maxScore:
+            maxScore = score
+            maxShift = count
+
+        windowFrequency = windowFrequency[1::] + [windowFrequency[0]]
+    
+    return maxShift
 
 def shiftString(inputStr: str):
     result = '0' + inputStr[:-1]
@@ -54,7 +77,6 @@ def keyLength(msg: str) -> list:
 
     return coincidences
 
-def vigenereDecryptKey(subkey: chr, subList: list) -> str:
     # Using Vigenere grid, decrypt the sublist (msg) using the subkey
     decryptedMsg = ""
     subkey = subkey.upper()
@@ -74,39 +96,36 @@ def vigenereDecryptKey(subkey: chr, subList: list) -> str:
 def vigenereDecrypt(key: str, ciphertext: str) -> str:
     # # Using Vigenere grid, decrypt the sublist (msg) using the subkey
     decryptedMsg = ""
+    # print("Key: " + key)
+    # print("CT: " + ciphertext + "\n")
     key = key.upper()
     key_len = len(key)
     
     for i, char in enumerate(ciphertext):
-        if char.isalpha():
-            row = ord(key[i % key_len]) - ord('A')
-            col = ord(char.upper()) - ord('A')
-            decryptedMsg += chr((col - row) % 26 + ord('A'))
-        else:
-            decryptedMsg += char
+        # print(i,char)
+        # if char.isalpha():
+        row = ord(key[i % key_len]) - ord('A')
+        col = ord(char.upper()) - ord('A')
+        decryptedMsg += chr((col - row) % 26 + ord('A'))
+        # else:
+        # decryptedMsg += char
 
     return decryptedMsg
-
-# vigenereDecrypt("C", ["B", "D", "Z"])
 
 def findSubKey(subList: list) -> chr:
     # Find Subkey from Sublist
     # list = (subKey, msgScore)
-    subKeyList = []
+    msgLen = len(subList)
+    msgFrequency = [ ('A', 0), ('B', 0), ('C', 0), ('D', 0), ('E', 0), ('F', 0), ('G', 0),
+    ('H', 0), ('I', 0), ('J', 0), ('K', 0), ('L', 0), ('M', 0), ('N', 0),
+    ('O', 0), ('P', 0), ('Q', 0), ('R', 0), ('S', 0), ('T', 0), ('U', 0),
+    ('V', 0), ('W', 0), ('X', 0), ('Y', 0), ('Z', 0)]
 
-    for subKey in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
-        decryptedSubMsg = vigenereDecrypt(subKey, subList)
-        subKeyList.append((scoreMsg(decryptedSubMsg), subKey))
+    for char in subList:
+        index = ord(char) - ord('A')
+        msgFrequency[index] = (char, msgFrequency[index][1] + 1/msgLen)
 
-    # from subKeyList, find max score and keep that subkey. 
-    max = 0
-    bestSubKey = ""
-    for sub in subKeyList:
-        if (sub[0] > max):
-            max = sub[0]
-            bestSubKey = sub[1]
-
-    return bestSubKey
+    return chr(scoreMsgMult(msgFrequency, msgLen) + ord("A"))
 
 def findKey(keyLength: int, msg: str) -> str:
     key = ''
@@ -122,16 +141,15 @@ def findKey(keyLength: int, msg: str) -> str:
 
     return key
 
-
 file = open("Lab0.TaskII.D.txt", "r")
 encryptedMSG = file.readline()
 encryptedMSG = encryptedMSG.strip("\n")
 file.close()
 
 listy = keyLength(encryptedMSG)
-print(findKey(14, encryptedMSG))
-
-key = "momoneyemoprobs"
-# print(key.upper(), encryptedMSG)
+# print(listy) # returns list with peaks that are keylength away from each other
+key = findKey(14, encryptedMSG)
+print(key)
+print()
 print(vigenereDecrypt(key.upper(), encryptedMSG))
 
